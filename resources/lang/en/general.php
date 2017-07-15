1dd0450383e0dd905c31d403a077c22aabcc5a50
Yoginth S <yoginth@hotmail.com>
2017-07-15 08:03:55 +0000

    CDN

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -54,8 +54,8 @@ return [
 	
 	  /* Content Delivery Network URL String */
 	
-    'url' => 'https://swapcdn.glitch.me/swapifier/assets/4.0.0/',
-	  'cdnurl' => 'https://swapcdn.glitch.me/swapifier/assets/4.0.0/',
+    'url' => 'https://swapcdn.glitch.me/swapifier/assets/5.0.0/',
+	  'cdnurl' => 'https://swapcdn.glitch.me/swapifier/assets/5.0.0/',
 
     /* Start new strings v1.2 */
     'blog' => 'Blog',
