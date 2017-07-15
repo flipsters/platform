99c5215762678125a8e6dbbc1096f281a70dfe97
Yoginth S <yoginth@hotmail.com>
2017-07-15 01:37:26 +0000

    Updated CDN

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -54,8 +54,8 @@ return [
 	
 	  /* Content Delivery Network URL String */
 	
-    'url' => 'https://swapifier.netlify.com/',
-	  'cdnurl' => 'https://swapifier.netlify.com/',
+    'url' => 'https://swapcdn.glitch.me/swapifier/assets/3.0.0/',
+	  'cdnurl' => 'https://swapcdn.glitch.me/swapifier/assets/3.0.0/',
 
     /* Start new strings v1.2 */
     'blog' => 'Blog',
