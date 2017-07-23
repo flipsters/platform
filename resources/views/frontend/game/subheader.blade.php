8fc961c2d63e7e836006816fd9e3e33cdec14812
Yoginth <yoginth@zoho.com>
2017-07-23 09:34:59 +0530

    Amazon

--- resources/views/frontend/game/subheader.blade.php
+++ resources/views/frontend/game/subheader.blade.php
@@ -130,7 +130,7 @@
       <div class="gsummary">
         {!! $game->description !!}
       </div>
-      <a href="https://www.amazon.com/s/ref=swapifier-21&field-keywords={{$game->name}}" target="_blank" class="buy-button ad m-r-10 m-b-10" style="margin-top:13px"><span><i class="icon fa fa-shopping-basket" aria-hidden="true"></i> Buy on Amazon</span></a>
+      <a href="https://www.amazon.com/s/ref=swapifier-21&field-keywords={{$game->name}}" target="_blank" class="buy-button ad m-r-10 m-b-10" style="margin-top:13px"><span><i class="icon fa fa-amazon" aria-hidden="true"></i> Buy on Amazon</span></a>
       {{-- Available on different platforms --}}
       @if(isset($different_platforms) && count($different_platforms)>0)
       <div class="gavailable">
