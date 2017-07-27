231247db80994b3df4cbec8c49cc5c00afd01eff
Yoginth <yoginth@zoho.com>
2017-07-27 20:58:17 +0530

    Indendeted

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -9,7 +9,7 @@
 -->
 <html class="no-js css-menubar" lang="{{config('settings.default_locale')}}" >
   <head>
-  	<link href="https://cdn.jsdelivr.net" rel="dns-prefetch" />
+  	 <link href="https://cdn.jsdelivr.net" rel="dns-prefetch" />
 	  <link href="https://i1.wp.com" rel="dns-prefetch" />
 	  <link rel="preload" href="{{ trans('general.cdnurl') }}img/logo.png" as="image">
 	
